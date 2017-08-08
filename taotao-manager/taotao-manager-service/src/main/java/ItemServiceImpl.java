1c7b9bdbb548bea06e1626a5ec891970fc1fd5e7
wushj <wushj@yonyou.com>
2017-08-08 18:19:32 +0800

    dubbo注册

---
+++ taotao-manager/taotao-manager-service/src/main/java/ItemServiceImpl.java
@@ -0,0 +1,17 @@
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        return itemMapper.selectByPrimaryKey(itemId);
+    }
+}
