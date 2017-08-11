52db45cac4a9efe38620a164bf542ac63ef3209d
wushj <wushj@yonyou.com>
2017-08-11 11:07:33 +0800

    商品分类,商品新增,商品列表查询,图片fastdfs上传

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,24 +1,48 @@
 package com.taotao.controller;
 
-import com.taotao.pojo.TbItem;
-import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+
+/**
+ * 商品管理Controller
+ * <p>Title: ItemController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
 @Controller
-@RequestMapping("/item")
 public class ItemController {
 
-    @Autowired
-    ItemService itemService;
-
-    @RequestMapping("/{itemId}")
-    @ResponseBody
-    public TbItem getItemById(@PathVariable long itemId) {
-        return itemService.getItemById(itemId);
-    }
-
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page, Integer rows) {
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		return result;
+	}
+	
+	@RequestMapping(value="/item/save", method=RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult addItem(TbItem item, String desc) {
+		TaotaoResult result = itemService.addItem(item, desc);
+		return result;
+	}
 }
