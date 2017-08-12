46e73cb364140be54074a0e419dae4651a5d08a6
wushj <wusjcoding@foxmail.com>
2017-08-12 23:42:52 +0800

    首页内容redis缓存

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
 import com.taotao.pojo.TbContentExample.Criteria;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,6 +21,12 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+
+	@Value("${INDEX_CONTENT}")
+	private String indexContent;
+
 
 	@Override
 	public TaotaoResult addContent(TbContent content) {
@@ -25,17 +35,41 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		//插入到内容表
 		contentMapper.insert(content);
+		//删除对应的缓存
+        jedisClient.hdel(indexContent,String.valueOf(content.getCategoryId()));
 		return TaotaoResult.ok();
 	}
 
 	@Override
 	public List<TbContent> getContentByCid(long cid) {
+		//先查询缓存
+		//添加缓存不能影响业务逻辑
+		try {
+		    //查询到结果转换成list返回
+            String result = jedisClient.hget(indexContent, String.valueOf(cid));
+            if (StringUtils.isNotBlank(result)){
+                List<TbContent> tbContents = JsonUtils.jsonToList(result, TbContent.class);
+                return tbContents;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+		}
+
+		//缓存未命中,查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		//设置查询条件
 		criteria.andCategoryIdEqualTo(cid);
 		//执行查询
 		List<TbContent> list = contentMapper.selectByExample(example);
+
+		//结果添加缓存
+		try {
+			jedisClient.hset(indexContent,String.valueOf(cid), JsonUtils.objectToJson(list));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 		//返回结果
 		return list;
 	}
--- taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedis.java
+++ taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedis.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
@@ -58,4 +60,16 @@ public class TestJedis {
 		//系统关闭前关闭JedisCluster
 		jedisCluster.close();
 	}
+
+	@Test
+	public void testJedisClient() throws Exception {
+		//初始化Spring容器
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		//从容器中获得JedisClient对象
+		JedisClient jedisClient = applicationContext.getBean(JedisClient.class);
+		jedisClient.set("first", "100");
+		String result = jedisClient.get("first");
+		System.out.println(result);
+	}
+
 }
