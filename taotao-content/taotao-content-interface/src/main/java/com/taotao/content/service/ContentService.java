2d761c46cdf88210399725c525de5a2022a69411
wushj <wushj@yonyou.com>
2017-08-12 16:55:13 +0800

    jedis 测试

--- taotao-content/taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
+++ taotao-content/taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
@@ -3,7 +3,11 @@ package com.taotao.content.service;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbContent;
 
+import java.util.List;
+
 public interface ContentService {
 
 	TaotaoResult addContent(TbContent content);
+
+    List<TbContent> getContentByCid(long cid);
 }
--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,21 +1,23 @@
 package com.taotao.content.service.impl;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentService;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
+import com.taotao.pojo.TbContentExample;
+import com.taotao.pojo.TbContentExample.Criteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
-	
+
 	@Override
 	public TaotaoResult addContent(TbContent content) {
 		//补全pojo的属性
@@ -24,6 +26,18 @@ public class ContentServiceImpl implements ContentService {
 		//插入到内容表
 		contentMapper.insert(content);
 		return TaotaoResult.ok();
+	}
+
+	@Override
+	public List<TbContent> getContentByCid(long cid) {
+		TbContentExample example = new TbContentExample();
+		Criteria criteria = example.createCriteria();
+		//设置查询条件
+		criteria.andCategoryIdEqualTo(cid);
+		//执行查询
+		List<TbContent> list = contentMapper.selectByExample(example);
+		//返回结果
+		return list;
 	}
 
 }
