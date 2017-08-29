fb800fabfcc7487466dab945827ee8d7d9e4d5f4
wushj <wushj@yonyou.com>
2017-08-29 14:52:39 +0800

    订单系统

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,7 +19,8 @@ public class PageController {
 		return "register";
 	}
 	@RequestMapping("/page/login")
-	public String showLogin() {
+	public String showLogin(String url ,Model model) {
+		model.addAttribute("redirect",url);
 		return "login";
 	}
 }
